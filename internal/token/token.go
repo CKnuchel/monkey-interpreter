34dda3c68444f166e36d448480660ab4149820eb
CKnuchel <christoph.knuchel@gmail.com>
2026-02-20 16:28:02 +0100

    Implement lexer and token structures for lexical analysis

---
+++ internal/token/token.go
@@ -0,0 +1,76 @@
+package token
+
+// Package token implements types and constants to support tokenizing
+// the input source before passing the stream of tokens on to the parser.
+
+const (
+	// ILLEGAL represents an illegal token
+	ILLEGAL = "ILLEGAL"
+
+	// EOF end of file
+	EOF = "EOF"
+
+	//
+	// Identifiers + literals
+	//
+
+	// IDENT an identifier, e.g: add, foobar, x, y, ...
+	IDENT = "IDENT"
+	// INT an integer, e.g: 1234
+	INT = "INT"
+
+	//
+	// Operators
+	//
+
+	// ASSIGN the assignment operator
+	ASSIGN = "="
+	// PLUS the addition operator
+	PLUS = "+"
+
+	//
+	// Delimiters
+	//
+
+	// COMMA a comma
+	COMMA = ","
+	// SEMICOLON a semi-colon
+	SEMICOLON = ";"
+
+	// LPAREN a left paranthesis
+	LPAREN = "("
+	// RPAREN a right parenthesis
+	RPAREN = ")"
+	// LBRACE a left brace
+	LBRACE = "{"
+	// RBRACE a right brace
+	RBRACE = "}"
+
+	//
+	// Keywords
+	//
+
+	// FUNCTION the `fn` keyword (function)
+	FUNCTION = "FUNCTION"
+	// LET the `let` keyword (let)
+	LET = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
