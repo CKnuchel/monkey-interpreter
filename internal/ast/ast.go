4bce77196a8804db06bc250e7251a625d5d8c449
CKnuchel <christoph.knuchel@gmail.com>
2026-02-20 17:53:08 +0100

    Add AST package for abstract syntax tree structures

---
+++ internal/ast/ast.go
@@ -0,0 +1,5 @@
+package ast
+
+// The ast package defines the abstract syntax tree (AST) structures that are used to represent the parsed source code. The AST is a hierarchical
+// representation of the program's structure, which can be used for
+// interpretation or compilation.
