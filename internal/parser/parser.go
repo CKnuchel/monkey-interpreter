ebc4387f6caef2f78cce8ffb5cd40b28454fdd5b
Christoph Knuchel <119663666+CKnuchel@users.noreply.github.com>
2026-02-23 06:40:59 +0000

    Implement parser structure and token management for Monkey language

---
+++ internal/parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"monkey-interpreter/internal/ast"
+	"monkey-interpreter/internal/lexer"
+	"monkey-interpreter/internal/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read tow tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
